01d04ce45d60b30402e59a09b0da5f4ab9dfdf58
Adria Tech <adriatech2020@gmail.com>
2023-12-15 11:18:25 +0100

    Board edit mode

--- lib/widgets/board.dart
+++ lib/widgets/board.dart
@@ -1,12 +1,18 @@
-// lib/widgets/board.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:board/widgets/board_column.dart';
 import 'package:board/models/board_model.dart';
 import 'package:board/models/board_column_model.dart';
 
-class Board extends StatelessWidget {
+class Board extends StatefulWidget {
   const Board({super.key});
+
+  @override
+  BoardState createState() => BoardState();
+}
+
+class BoardState extends State<Board> {
+  bool _isEditMode = false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +22,20 @@ class Board extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(config.title),
+        actions: [
+          IconButton(
+            icon: Icon(_isEditMode ? Icons.check : Icons.edit),
+            onPressed: _toggleEditMode,
+            tooltip: _isEditMode ? 'Finish Editing' : 'Edit Layout',
+          ),
+        ],
       ),
       body: Container(
         color: config.backgroundColor,
         child: AnimatedList(
           key: boardModel.listKey,
           scrollDirection: Axis.horizontal,
-          initialItemCount: config.columns.length + 1,
+          initialItemCount: config.columns.length + (_isEditMode ? 1 : 0),
           itemBuilder: (context, index, animation) {
             if (index < config.columns.length) {
               var columnModel = BoardColumnModel();
@@ -37,6 +50,25 @@ class Board extends StatelessWidget {
     );
   }
 
+  void _toggleEditMode() {
+    setState(() {
+      _isEditMode = !_isEditMode;
+    });
+    _updateList();
+  }
+
+  void _updateList() {
+    final boardModel = Provider.of<BoardModel>(context, listen: false);
+    if (_isEditMode) {
+      boardModel.listKey.currentState?.insertItem(boardModel.configuration.columns.length);
+    } else {
+      boardModel.listKey.currentState?.removeItem(
+        boardModel.configuration.columns.length,
+        (context, animation) => _buildAddColumnButton(context),
+      );
+    }
+  }
+
   Widget _buildAnimatedColumn(BoardColumnModel model, Animation<double> animation) {
     return FadeTransition(
       opacity: animation,
@@ -45,14 +77,17 @@ class Board extends StatelessWidget {
   }
 
   Widget _buildAddColumnButton(BuildContext context) {
-    final boardModel = Provider.of<BoardModel>(context, listen: false);
-    return Container(
-      alignment: Alignment.topCenter,
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      child: OutlinedButton(
-        onPressed: () => boardModel.addNewColumn(),
-        child: const Text('Add Column'),
-      ),
-    );
+    if (_isEditMode) {
+      final boardModel = Provider.of<BoardModel>(context, listen: false);
+      return Container(
+        alignment: Alignment.topCenter,
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: OutlinedButton(
+          onPressed: () => boardModel.addNewColumn(),
+          child: const Text('Add Column'),
+        ),
+      );
+    }
+    return const SizedBox.shrink();
   }
 }
