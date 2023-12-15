b35cdce1cc8c064ad2555d45009821da4f2b52f7
Adria Tech <adriatech2020@gmail.com>
2023-12-15 10:24:17 +0100

    board id

--- lib/config/board_configuration.dart
+++ lib/config/board_configuration.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:board/config/column_configuration.dart';
 import 'package:board/config/board_defaults.dart';
+import 'package:uuid/uuid.dart';
 
 class BoardConfiguration {
+  final String id;
   final String title;
   final Color backgroundColor;
   final List<ColumnConfiguration> columns;
 
   BoardConfiguration({
+    String? id,
     required this.title,
     required this.backgroundColor,
     required this.columns,
-  });
+  }): id = id ?? _generateBoardId();
+
+  static String _generateBoardId() {
+    var uuid = const Uuid();
+    return '${DateTime.now().millisecondsSinceEpoch}-${uuid.v4()}';
+  }
 
   // Method to merge custom config with default config
   static BoardConfiguration getConfig([Map<String, dynamic>? customConfig]) {
